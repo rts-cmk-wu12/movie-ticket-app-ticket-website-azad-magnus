19f43c9df94675682c3afdd1588d71e5a43b6491
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-19 10:42:12 +0200

    Added template Files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,11 @@
+import {ActionButton} from "~components/ActionButton.jsx";
+
 const mainPage = () => {
     return (
         <div>
             <h2>Hello World!</h2>
+            <ActionButton text={"Checkout"} anchorTagClass={"standard"} buttonClass={"standard__button"}></ActionButton>
+            <ActionButton text={"Go Back To Page"} anchorTagClass={"backButton"} buttonClass={"backButton__button"}></ActionButton>
         </div>
     )
 }
