4e726f950ede875a8f2467cf4944c68296d1cc9b
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-19 09:14:51 +0200

    Added Files

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+const mainPage = () => {
+    return (
+        <div>
+            <h2>Hello World!</h2>
+        </div>
+    )
+}
+
+export default mainPage;
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import Pages from "vite-plugin-pages"
+import {ViteAliases} from "vite-aliases";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), ViteAliases(), Pages()],
+})
