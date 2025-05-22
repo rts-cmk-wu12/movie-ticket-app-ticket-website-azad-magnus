71e5cfede431489d91becc824eb3d650449b2764
Azad <u2483788@rts-365.dk>
2025-05-22 12:28:04 +0200

    headernavigation added to detali page

--- src/components/HeaderNavigation.jsx
+++ src/components/HeaderNavigation.jsx
@@ -2,12 +2,13 @@ import { MdArrowBackIos } from "react-icons/md";
 import { Link } from "react-router";
 import "~style/components/HeaderNavigation.scss";
 
-export const HeaderNavigation = ({title , link}) => {
+export const HeaderNavigation = ({title , link, element}) => {
     return (
         <>
         <div className="header">
            <Link to={link}><MdArrowBackIos className="arrowback"/></Link>
            <h1 className="headline-in-settings">{title}</h1>
+           {element}
            </div>
         </>
     )
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import {ActionButton} from "~components/ActionButton.jsx";
+import { HeaderNavigation } from "~components/HeaderNavigation";
 import {NavigationBar} from "~components/NavigationBar.jsx";
 
 const discoverPage = () => {
@@ -6,6 +7,7 @@ const discoverPage = () => {
         <>
             <ActionButton text={"Checkout"} anchorTagClass={"standard"} buttonClass={"standard__button"}></ActionButton>
             <ActionButton text={"Go Back To Page"} anchorTagClass={"backButton"} buttonClass={"backButton__button"}></ActionButton>
+            <HeaderNavigation title={"Explore"} link={"/home"} element={<h2>Hej</h2>}></HeaderNavigation>
             <NavigationBar></NavigationBar>
         </>
     )
