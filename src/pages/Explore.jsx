415192409859e2e2109eff9b0135ba2a1d517a82
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-23 10:07:16 +0200

    Changed

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,13 +1,62 @@
-import {ActionButton} from "~components/ActionButton.jsx";
-import { HeaderNavigation } from "~components/HeaderNavigation";
+import {HeaderNavigation} from "~components/HeaderNavigation";
 import {NavigationBar} from "~components/NavigationBar.jsx";
+import {IoIosSearch} from "react-icons/io";
+import "~style/components/Explore.scss"
+import {useEffect, useState} from "react";
+import {Link} from "react-router";
+
 
 const discoverPage = () => {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchPopularMovies = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/movie/popular');
+                const data = await response.json();
+                setMovies(data.results);
+            } catch (error) {
+                console.error('Failed to fetch movies:', error);
+            }
+        };
+
+        fetchPopularMovies();
+    }, []);
+
+
     return (
         <>
-            <ActionButton text={"Checkout"} anchorTagClass={"standard"} buttonClass={"standard__button"}></ActionButton>
-            <ActionButton text={"Go Back To Page"} anchorTagClass={"backButton"} buttonClass={"backButton__button"}></ActionButton>
-            <HeaderNavigation title={"Explore"} link={"/home"} element={<h2>Hej</h2>}></HeaderNavigation>
+            <HeaderNavigation title={"Explore"} link={"/"} element={<IoIosSearch size={24}/>}></HeaderNavigation>
+
+            <div className={"top-movies"}>
+                <div className={"top-movies__heading"}>
+                    <h2>Top Movies</h2>
+                    <p>See more</p>
+                </div>
+                <div className="top-movies__section">
+                    {movies.map((movie) => (
+                        <div key={movie.id} className="top-movies__movie">
+                            <Link to={`/details/${movie.id}`}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                alt={movie.title}
+                                className="top-movies__image"
+                            />
+                            </Link>
+                            <p className="top-movies__title">{movie.title}</p>
+                            <div className="top-movies__stars">
+                                <div
+                                    style={{width: `${(movie.vote_average / 10) * 100}%`}}
+                                    className="top-movies__rating"
+                                >
+                                    <span>★★★★★</span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
             <NavigationBar></NavigationBar>
         </>
     )
