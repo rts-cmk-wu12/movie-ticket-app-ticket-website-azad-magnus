2508c6dc4dbff0c048e193517261b7addf2df42c
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-27 08:54:04 +0200

    Added explore with new recommended section but api doesent have recommended so we just fetched the previously thing.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -9,12 +9,18 @@ import {Link} from "react-router";
 const discoverPage = () => {
 
     const [movies, setMovies] = useState([]);
+    const [genres, setGenres] = useState([]);
 
     useEffect(() => {
         const fetchPopularMovies = async () => {
             try {
                 const response = await fetch('http://localhost:3000/movie/popular');
+                const genreResponse = await fetch("http://localhost:3000/genre/movie/list")
+
                 const data = await response.json();
+                const genreData = await genreResponse.json()
+                console.log(genreData.genres)
+                setGenres(genreData.genres);
                 setMovies(data.results);
             } catch (error) {
                 console.error('Failed to fetch movies:', error);
@@ -38,11 +44,11 @@ const discoverPage = () => {
                     {movies.map((movie) => (
                         <div key={movie.id} className="top-movies__movie">
                             <Link to={`/details/${movie.id}`}>
-                            <img
-                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                alt={movie.title}
-                                className="top-movies__image"
-                            />
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    alt={movie.title}
+                                    className="top-movies__image"
+                                />
                             </Link>
                             <p className="top-movies__title">{movie.title}</p>
                             <div className="top-movies__stars">
@@ -53,6 +59,29 @@ const discoverPage = () => {
                                     <span>★★★★★</span>
                                 </div>
                             </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={"recommend"}>
+                <div className={"recommend__heading"}>
+                    <h2>Recommended</h2>
+                    <p>See more</p>
+                </div>
+                <div className={"recommend__section"}>
+                    {movies.map((movie) => (
+                        <div key={movie.id} className="recommend__section__movie">
+                            <Link to={`/details/${movie.id}`}>
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    alt={movie.title}
+                                    className="recommend__section__image"
+                                />
+                            </Link>
+                            <p className="recommend__section__title">{movie.title}</p>
+                            <p className="recommend__section__genre">
+                                {genres.find((genre) => genre.id === movie.genre_ids[0])?.name || 'Unknown'}
+                            </p>
                         </div>
                     ))}
                 </div>
